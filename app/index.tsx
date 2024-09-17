90fec54a2fbfd6e2f4f915ace70dd593da8dc065
cardbo <william.tweed@hotmail.com>
2024-09-17 18:13:33 +0100

    started new format!

--- app/index.tsx
+++ app/index.tsx
@@ -8,6 +8,8 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 
+
+
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
@@ -22,7 +24,7 @@ var timePeriodvariable = 1;
 
 
 export default function Index() {
-  const [date, setDate] = useState(new Date());
+  
   useLocalNotification();
   const handleLocalPushNotification = async() => {
     await schedulePushNotification(timePeriodvariable);
@@ -48,23 +50,41 @@ export default function Index() {
       <ThemedView>
 
       <Collapsible title="mONTAG">
+        <ThemedText type='subtitle'>
+          8:35 - 9:00
+        </ThemedText>
       <ThemedText>
-        8:35 form time
+        form time {'\n'}
       </ThemedText>
+      <ThemedText type='subtitle'>
+          9:00 - break
+        </ThemedText>
       <ThemedText>
-        9:00 - break double physics  
+        double physics {'\n'}
       </ThemedText>
+      <ThemedText type='subtitle'>
+          10:45 - 11:10
+        </ThemedText>
       <ThemedText>
-        10:45 - 11:10 break
+        break {'\n'}
       </ThemedText>
+      <ThemedText type='subtitle'>
+          11:10 - 12:00
+        </ThemedText>
       <ThemedText>
-        11:10 - 12:00 freee !
+        freee ! {'\n'}
       </ThemedText>
+      <ThemedText type='subtitle'>
+          12:00 - lunch
+        </ThemedText>
       <ThemedText>
-        12:00 - lunch math (amlani) (lecture rooms)  
+        math (amlani) (lecture rooms) {'\n'}
       </ThemedText>
+      <ThemedText type='subtitle'>
+          12:55 - 1:30
+        </ThemedText>
       <ThemedText>
-        12:55 - 1:30 eat luncheon
+        eat lunchean {'\n'}
       </ThemedText>
       <ThemedText>
         1:30 - 2:10 goto math club  
@@ -140,11 +160,82 @@ export default function Index() {
 
       </ThemedView>  
 
+      <ThemedView>
+
+      <Collapsible title="donners tag">
+      <ThemedText>
+        8:35 form time
+      </ThemedText>
+      <ThemedText>
+        9:00 - break compute (it1)
+      </ThemedText>
+      <ThemedText>
+        10:45 - 11:10 break
+      </ThemedText>
+      <ThemedText>
+        11:10 - 12:00 physics (moyes)
+      </ThemedText>
+      <ThemedText>
+        12:00 - 12:55 freee (yay!)
+      </ThemedText>
+      <ThemedText>
+        12:55 - 2:10 eat luncheon
+      </ThemedText>
+      <ThemedText>
+        2:10 - 3:05 math (amlan) (lecturerooms)
+      </ThemedText>
+      <ThemedText>
+        3:05 - end math (barg) (m1)
+      </ThemedText>
+
+
+      </Collapsible>
+
+      </ThemedView>  
+
+
+      <ThemedView>
+
+      <Collapsible title="day before saturday">
+      <ThemedText>
+        8:35 form time
+      </ThemedText>
+      <ThemedText>
+        9:00 - 9:50 math (amlan) (lecturerooms)
+      </ThemedText>
+      <ThemedText>
+        9:50 - 10:45 math (myer) (room down the hall)
+      </ThemedText>
+      <ThemedText>
+        10:45 - 11:10 prefECT DUtY
+      </ThemedText>
+      <ThemedText>
+        11:10 - lunch double compute (mattu) (it1)
+      </ThemedText>
+      <ThemedText>
+        12:55 - 1:30 eat luncheon
+      </ThemedText>
+      <ThemedText>
+        1:30 - 2:10 john cipher (it2)
+      </ThemedText>
+      <ThemedText>
+        2:10 - 3:05 physics (blaker)
+      </ThemedText>
+      <ThemedText>
+        3:05 - end interest options (changes)
+      </ThemedText>
+
+
+      </Collapsible>
+
+      </ThemedView>  
+
+
 
 
       <Pressable style={styles.button} onPress={handleLocalPushNotification}>
       
-      <Text>{"do NOT press this" }</Text>
+      <Text>{ "do NOT press this" }</Text>
       
     </Pressable>      
     
@@ -180,9 +271,3 @@ const styles = StyleSheet.create({
   },
 
 });
-
-
-
-
-
-
